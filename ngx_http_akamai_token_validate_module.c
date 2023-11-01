4ca9f51d68bf3907f526b6ab601489ff516c059a
Krivorot Oleg <krivorot.oleg@gmail.com>
2023-11-02 01:24:58 +0200

    add config parameter: akamai_token_validate_str_ip

--- ngx_http_akamai_token_validate_module.c
+++ ngx_http_akamai_token_validate_module.c
@@ -83,7 +83,7 @@ static ngx_command_t  ngx_http_akamai_token_validate_commands[] = {
 
         { ngx_string("akamai_token_validate_str_ip"),
           NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1,
-          ngx_conf_set_str_slot,
+          ngx_http_set_complex_value_slot,
           NGX_HTTP_LOC_CONF_OFFSET,
           offsetof(ngx_http_akamai_token_validate_loc_conf_t, ip),
           NULL },
